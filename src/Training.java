5c53c2aa42671eac6787142d01c5906ee6dffd21
ragavendran <mynameisragav@gmail.com>
2015-06-18 12:58:52 +0530

    Pull Commit

--- src/Training.java
+++ src/Training.java
@@ -4,7 +4,13 @@ public class Training {
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println("hi");
 	}
 
+	 public sample()
+	 {
+		 
+		 System.out.println("im sample method"); 
+		 
+	 }
 }
